87d2ccabf7883cd12df0118543232b71be3c6a7c
aperez809 <32823645+aperez809@users.noreply.github.com>
2019-01-19 09:39:26 -0500

    added bones of API wrapper and web app

--- api_wrapper.py
+++ api_wrapper.py
@@ -1,17 +1,34 @@
 import json
 import requests
+from pprint import pprint
 
 RESTAURANTS = {each["name"] : each["id"] for each in json.loads(requests.get(
                     "https://d1gvlspmcma3iu.cloudfront.net/restaurants-3d-party.json.gz").content)}
 
 class API:
 
-    def __init__(self, app_id, app_key, user_id=0):
+    def __init__(self, app_id, app_key, user_id=str(0), base_url="https://trackapi.nutritionix.com/"):    #"https://api.nutritionix.com/v1_1"):
         self.app_id = app_id
         self.app_key = app_key
         self.user_id = user_id
+        self.base_url = base_url
+
+    def get_food_items(self, restaurant_id):
+        return json.loads(requests.get("https://api.nutritionix.com/v1_1/brand",
+                     params={"appId": self.app_id,
+                             "appKey": self.app_key,
+                             "brand_ids": (restaurant_id)}).content)
 
 
+    def get_food_items_v2(self, restaurant_name):
+        return json.loads(requests.get(f"{self.base_url}/v2/search/instant",
+                                       headers= {"x-app-id": self.app_id,
+                                                 "x-app-key": self.app_key},
+                                       params={"query": ,
+                                               "brand_ids": (RESTAURANTS[restaurant_name])}).content)
+
+conn = API("c6065fe5", "e02df01235e480620daf9a737a4d520a")
+pprint(conn.get_food_items("Qdoba"))
 
 
 
--- app.py
+++ app.py
@@ -3,12 +3,20 @@ from flask import Flask, render_template
 import api_wrapper
 
 app = Flask(__name__)
-
+API_CREDS = api_wrapper.API("c6065fe5", "e02df01235e480620daf9a737a4d520a")
 
 @app.route('/', methods=["GET"])
 def main_page():
     restaurants = api_wrapper.RESTAURANTS
-    return render_template('index.html', restaurants=sorted(restaurants.keys()))
+    available_ingreds = API_CREDS.get_food_items_v2()
+
+    chosen_ingreds = [""] * 5
+
+    return render_template('index.html', restaurants=sorted(restaurants.keys()), chosen_ingreds=chosen_ingreds)
+
+@app.route('/add_row')
+def add_row():
+    pass
 
 
 if __name__ == '__main__':
