ae8d5f2335a0b4cbad581c390e3aa7c431b84d35
aperez809 <32823645+aperez809@users.noreply.github.com>
2019-01-24 01:07:42 -0500

    removed API key from public view

--- app.py
+++ app.py
@@ -3,16 +3,13 @@ from flask import Flask, render_template
 import api_wrapper
 
 app = Flask(__name__)
-API_CREDS = api_wrapper.API("c6065fe5", "e02df01235e480620daf9a737a4d520a")
 
 @app.route('/', methods=["GET"])
 def main_page():
     restaurants = api_wrapper.RESTAURANTS
-    available_ingreds = API_CREDS.get_food_items_v2()
+    available_ingreds = api_wrapper.get_food_items_v2()
 
-    chosen_ingreds = [""] * 5
-
-    return render_template('index.html', restaurants=sorted(restaurants.keys()), chosen_ingreds=chosen_ingreds)
+    return render_template('index.html', restaurants=sorted(restaurants.keys()), available_ingreds=available_ingreds)
 
 @app.route('/add_row')
 def add_row():
